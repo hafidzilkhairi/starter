82880c3e779cce597f92d66e07ca508f444f0d55
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2024-06-02 21:37:48 +0700

    add relative path to statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,6 +6,39 @@ local M = {}
 
 M.ui = {
 	theme = "ashes",
+  statusline = {
+    modules = {
+      file = function()
+        local default_sep_icons = {
+          default = { left = "", right = " " },
+          round = { left = "", right = "" },
+          block = { left = "█", right = "█" },
+          arrow = { left = "", right = "" },
+        }
+        local config = require "nvconfig"
+        local sep_style = config.ui.statusline.theme
+
+        local separators = (type(sep_style) == "table" and sep_style) or default_sep_icons[sep_style]
+
+        local sep_r = separators["right"]
+        local icon = " 󰈚 "
+        local name = vim.fn.expand("%:.")
+
+        if name ~= "Empty " then
+          local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+          if devicons_present then
+            local ft_icon = devicons.get_icon(name)
+            icon = (ft_icon ~= nil and " " .. ft_icon) or ""
+          end
+
+          name = " " .. name .. " "
+        end
+
+        return "%#StText# " .. icon .. name .. "%#St_file_sep#" .. sep_r
+      end,
+    }
+  }
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
